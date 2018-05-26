b60e503aff5859f1a1417e4a65367f9a6fbf7c75
Eduardo_Cabral <theduardds@gmail.com>
2018-05-25 23:55:14 -0300

    organize files

--- core_frontend/app/app-config.service.js
+++
@@ -1,8 +0,0 @@
-app.service('configService', function configService() {
-	return {
-		urlApi: 'http://localhost:8080/'
-		//urlApi: 'http://192.168.137.245:8080/'
-	}
-});
-
-	
--- core_frontend/app/app.module.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('core', 
-						[	
-							'ngRoute',
-							'toastr'
-						]);
